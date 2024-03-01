47dffc1f2c7aef66bb1c21c0bcc077d2ce7a73fc
fm528 <fm528@cam.ac.uk>
2024-03-01 11:33:28 +0000

    Final changes I hope

--- src/boot/ksdk1.1.0/boot.c
+++ src/boot/ksdk1.1.0/boot.c
@@ -3605,13 +3605,13 @@ printAllSensors(bool printHeadersAndCalibration, bool hexModeFlag,
 #endif
 
 #if (WARP_BUILD_ENABLE_DEVINA219)
-		warpPrint(" INA219 Current ");
+		warpPrint(" INA219 Current, INA219 Voltage, INA219 Power,");
 #endif
 
 #if (WARP_CSVSTREAM_FLASH_PRINT_METADATA)
 		warpPrint(" RTC->TSR, RTC->TPR,");
 #endif
-		warpPrint(" numberOfConfigErrors");
+		warpPrint(" time");
 		warpPrint("\n\n");
 	}
 
@@ -3667,7 +3667,7 @@ printAllSensors(bool printHeadersAndCalibration, bool hexModeFlag,
 #if (WARP_CSVSTREAM_FLASH_PRINT_METADATA)
 		warpPrint(" %12d, %6d,", RTC->TSR, RTC->TPR);
 #endif
-		warpPrint(" %u\n", numberOfConfigErrors);
+		warpPrint(" %u\n", OSA_TimeGetMsec());
 
 		if (menuDelayBetweenEachRun > 0)
 		{
@@ -3913,7 +3913,7 @@ loopForSensor2bytes(	const char *  tagString,
 
 						if (chatty)
 						{
-						warpPrint("\r\t0x%02x --> 0x%02x 0x%02x\n",
+						warpPrint("\r\t0x%02x --> %d\n",
 							address+j,
 									  i2cDeviceState->i2cBuffer[0],
                                       i2cDeviceState->i2cBuffer[1]);
